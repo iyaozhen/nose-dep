065fdc8cb72a8bba3ac32d31955e883802aa2241
yaozhen <yaozhen@baidu.com>
2021-02-09 15:33:46 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -20,11 +20,10 @@ setup_args = dict(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Testing',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
     )
 
